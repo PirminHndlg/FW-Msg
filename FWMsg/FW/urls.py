b5dbd0fadb7a8bf3e6dd34d35353c945933a5ef4
Pirmin <pirmin@absinth.net>
2024-10-23 16:19:39 +0200

    footer position, menu bar, changed the url for fw home

--- FWMsg/FW/urls.py
+++ FWMsg/FW/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='fwhome'),
     path('profil/', views.profil, name='profil'),
     path('ampel/', views.ampel, name='ampel'),
     path('aufgaben/', views.aufgaben, name='aufgaben'),
