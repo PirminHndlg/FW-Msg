37a24e1ce778e2ecda311b87ef1efb30bf0cad89
Pirmin <pirmin@absinth.net>
2024-10-26 23:19:34 +0200

    static paths

--- FWMsg/FWMsg/settings.py
+++ FWMsg/FWMsg/settings.py
@@ -122,6 +122,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'FW/fw-static',
+    BASE_DIR / 'ORG/org-static',
+    BASE_DIR / 'logos',
 ]
 
 LOGIN_REDIRECT_URL = '/'
