d06c468733734e2cc5f399151dc53ffa3deb2423
Pirmin <pirmin@absinth.net>
2024-10-26 09:40:15 +0200

    kirchenzugehoerigkeit model added, if ende_real chaned change the aufgaben faellig

--- FWMsg/FW/models.py
+++ FWMsg/FW/models.py
@@ -1,3 +1,6 @@
+import os
+from datetime import timedelta
+
 from django.contrib.auth.models import User
 from django.db import models
 from ORG.models import Organisation
@@ -32,6 +35,17 @@ class Entsendeform(models.Model):
     class Meta:
         verbose_name = 'Entsendeform'
         verbose_name_plural = 'Entsendeformen'
+
+    def __str__(self):
+        return self.name
+
+class Kirchenzugehoerigkeit(models.Model):
+    org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Kirchenzugehörigkeit'
+        verbose_name_plural = 'Kirchenzugehörigkeiten'
 
     def __str__(self):
         return self.name
@@ -105,7 +119,7 @@ class Freiwilliger(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     phone_einsatzland = models.CharField(max_length=20, blank=True, null=True)
     entsendeform = models.ForeignKey(Entsendeform, on_delete=models.SET_NULL, null=True, blank=True)
-    ekbo_anhalt = models.BooleanField(default=False)
+    kirchenzugehoerigkeit = models.ForeignKey(Kirchenzugehoerigkeit, on_delete=models.SET_NULL, null=True, blank=True)
     start_geplant = models.DateField(blank=True, null=True)
     start_real = models.DateField(blank=True, null=True)
     ende_geplant = models.DateField(blank=True, null=True)
@@ -114,6 +128,17 @@ class Freiwilliger(models.Model):
     class Meta:
         verbose_name = 'Freiwillige:r'
         verbose_name_plural = 'Freiwillige'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._original_start_real = self.start_real
+        self._original_ende_real = self.ende_real
+
+    def has_field_changed(self, field_name):
+        """Helper to check if a field has changed."""
+        original_value = getattr(self, f"_original_{field_name}")
+        current_value = getattr(self, field_name)
+        return original_value != current_value
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
@@ -143,6 +168,20 @@ def post_save_handler(sender, instance, created, **kwargs):
         # Link the created User to the Freiwilliger instance
         instance.user = user
         instance.save()
+
+    else:
+        if instance.has_field_changed('ende_real'):
+            tasks = FreiwilligerAufgaben.objects.filter(freiwilliger=instance, faellig__isnull=False,
+                                                        aufgabe__faellig_tage_vor_ende__gt=0)
+            for task in tasks:
+                task.faellig = instance.ende_real - timedelta(days=task.aufgabe.faellig_tage_vor_ende)
+                task.save()
+        if instance.has_field_changed('start_real'):
+            tasks = FreiwilligerAufgaben.objects.filter(freiwilliger=instance, faellig__isnull=False,
+                                                        aufgabe__faellig_tage_nach_start__gt=0)
+            for task in tasks:
+                task.faellig = instance.start_real + timedelta(days=task.aufgabe.faellig_tage_nach_start)
+                task.save()
 
 
 @receiver(post_delete, sender=Freiwilliger)
@@ -195,12 +234,39 @@ class Aufgabenprofil(models.Model):
 
 
 class FreiwilligerAufgaben(models.Model):
+    WIEDERHOLUNG_CHOICES = [
+        ('T', 'Täglich'),
+        ('W', 'Wöchentlich'),
+        ('M', 'Monatlich'),
+        ('J', 'Jährlich'),
+        ('N', 'Nicht wiederholen')
+    ]
+
     freiwilliger = models.ForeignKey(Freiwilliger, on_delete=models.CASCADE)
     aufgabe = models.ForeignKey('Aufgabe', on_delete=models.CASCADE)
     erledigt = models.BooleanField(default=False)
     pending = models.BooleanField(default=False)
     datetime = models.DateTimeField(auto_now_add=True)
     faellig = models.DateField(blank=True, null=True)
+    wiederholung = models.CharField(max_length=1, choices=WIEDERHOLUNG_CHOICES)
+    wiederholung_ende = models.DateField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        print(self.wiederholung, self.wiederholung_ende)
+        if self.wiederholung != 'N' and not self.wiederholung_ende:
+            self.wiederholung_ende = self.freiwilliger.ende_geplant
+        if not self.faellig:
+
+            if self.aufgabe.faellig_tage_nach_start:
+                self.faellig = self.freiwilliger.start_geplant + timedelta(days=self.aufgabe.faellig_tage_nach_start)
+            elif self.aufgabe.faellig_tage_vor_ende:
+                self.faellig = self.freiwilliger.ende_geplant - timedelta(days=self.aufgabe.faellig_tage_vor_ende)
+            else:
+                self.faellig = self.aufgabe.faellig
+
+            # if self.aufgabe.faellig:
+            #     self.faellig = self.aufgabe.faellig
+        super(FreiwilligerAufgaben, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Freiwilliger Aufgabe'
@@ -208,6 +274,39 @@ class FreiwilligerAufgaben(models.Model):
 
     def __str__(self):
         return self.freiwilliger.first_name + ' ' + self.freiwilliger.last_name + ' - ' + self.aufgabe.name
+
+
+class FreiwilligerUpload(models.Model):
+    org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    freiwilligeraufgabe = models.ForeignKey(FreiwilligerAufgaben, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Freiwilliger Upload'
+        verbose_name_plural = 'Freiwilliger Uploads'
+
+    def __str__(self):
+        return self.file.name
+
+
+# @receiver(post_delete, sender=FreiwilligerUpload)
+# def post_delete_handler(sender, instance, **kwargs):
+#     instance.file.delete(False)
+#     os.remove(instance.file.path)
+
+class FreiwilligerFotos(models.Model):
+    org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    freiwilliger = models.ForeignKey(Freiwilliger, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='fotos/')
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Freiwilliger Foto'
+        verbose_name_plural = 'Freiwilliger Fotos'
+
+    def __str__(self):
+        return self.file.name
 
 
 class FreiwilligerAufgabenprofil(models.Model):
@@ -241,6 +340,9 @@ class Aufgabe(models.Model):
     org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     beschreibung = models.TextField(null=True, blank=True)
+    faellig = models.DateField(blank=True, null=True)
+    faellig_tage_nach_start = models.IntegerField(blank=True, null=True)
+    faellig_tage_vor_ende = models.IntegerField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Aufgabe'
