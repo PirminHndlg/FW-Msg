225880635801cac241364e5a58ae952976f1c906
Pirmin <pirmin@absinth.net>
2024-11-03 21:22:04 +0100

    added dokumente to admin

--- FWMsg/ORG/admin.py
+++ FWMsg/ORG/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Organisation
+from .models import Organisation, Ordner, Dokument
 
 # Register your models here.
 @admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     search_fields = ['name']
+
+@admin.register(Ordner)
+class OrdnerAdmin(admin.ModelAdmin):
+    search_fields = ['ordner_name']
+
+@admin.register(Dokument)
+class DokumentAdmin(admin.ModelAdmin):
+    search_fields = ['ordner', 'dokument', 'beschreibung']
