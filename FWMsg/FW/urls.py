dd4c2503c8832e29d40c542a439edb56fc243aa8
Pirmin <pirmin@absinth.net>
2024-11-03 13:15:57 +0100

    added urls for bilder

--- FWMsg/FW/urls.py
+++ FWMsg/FW/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('aufgaben/', views.aufgaben, name='aufgaben'),
     path('aufgaben/<int:aufgabe_id>/', views.aufgabe, name='aufgaben_detail'),
     path('logos/<str:image_name>', views.serve_logo, name='serve_image'),
-
+    path('bilder/', views.bilder, name='bilder'),
+    path('bilder/<str:image_name>', views.serve_bilder, name='serve_bilder'),
+    path('bild/', views.bild, name='bild'),
 ]
