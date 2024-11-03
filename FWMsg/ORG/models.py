937fa857e20a6b731c7b14f91eeba01dd88c1c63
Pirmin <pirmin@absinth.net>
2024-11-03 21:22:15 +0100

    added dokumente to models

--- FWMsg/ORG/models.py
+++ FWMsg/ORG/models.py
@@ -1,4 +1,9 @@
+import os.path
+
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Organisation(models.Model):
@@ -30,3 +35,59 @@ class MailBenachrichtigungen(models.Model):
 
     def __str__(self):
         return f'{self.organisation} - {self.betreff}'
+
+
+class Ordner(models.Model):
+    org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    ober_ordner = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    ordner_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ordner_name
+
+
+@receiver(post_save, sender=Ordner)
+def create_folder(sender, instance, **kwargs):
+    order = instance.ober_ordner
+    path = os.path.join(instance.ordner_name)
+    while order:
+        path = os.path.join(order.ordner_name, path)
+        order = order.ober_ordner
+    os.makedirs(os.path.join(instance.org.name, path), exist_ok=True)
+
+
+@receiver(post_delete, sender=Ordner)
+def remove_folder(sender, instance, **kwargs):
+    order = instance.ober_ordner
+    path = os.path.join(instance.ordner_name)
+    while order:
+        path = os.path.join(order.ordner_name, path)
+        order = order.ober_ordner
+    path = os.path.join(instance.org.name, path)
+    if os.path.isdir(path):
+        os.rmdir(path)
+
+
+def upload_to_folder(instance, filename):
+    order = instance.ordner
+    path = os.path.join(filename)
+    while order:
+        path = os.path.join(order.ordner_name, path)
+        order = order.ober_ordner
+    return os.path.join(instance.org.name, path)
+
+class Dokument(models.Model):
+    org = models.ForeignKey(Organisation, on_delete=models.CASCADE)
+    ordner = models.ForeignKey(Ordner, on_delete=models.CASCADE)
+    dokument = models.FileField(upload_to=upload_to_folder)
+    date_created = models.DateTimeField(auto_now_add=True)
+    beschreibung = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.dokument.name
+
+
+@receiver(post_delete, sender=Dokument)
+def remove_file(sender, instance, **kwargs):
+    if os.path.isfile(instance.dokument.path):
+        os.remove(instance.dokument.path)
