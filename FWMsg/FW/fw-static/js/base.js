a7662e860e6b0af09b61bbed0848c6ea62c8a6cb
Pirmin <pirmin@absinth.net>
2024-11-05 15:04:28 +0100

    show img big

--- FWMsg/FW/fw-static/js/base.js
+++ FWMsg/FW/fw-static/js/base.js
@@ -15,3 +15,33 @@ function onDarkBgClicked() {
     let dark_bg = document.getElementById('dark_bg');
     dark_bg.style.display = 'none';
 }
+
+function showImgLarge(img) {
+    let img_data = img.src
+    let img_large_div = document.createElement('div');
+    let img_large = document.createElement('img');
+    img_large.src = img_data;
+    img_large.style.width = 'auto';
+    img_large.style.height = 'auto';
+    img_large.style.maxWidth = '90%';
+    img_large.style.maxHeight = '90%';
+    img_large.style.objectFit = 'contain';
+
+    img_large_div.appendChild(img_large);
+
+    img_large_div.style.display = 'flex';
+    img_large_div.style.justifyContent = 'center';
+    img_large_div.style.alignItems = 'center';
+    img_large_div.style.width = '100%';
+    img_large_div.style.height = '100%';
+    img_large_div.style.position = 'fixed';
+    img_large_div.style.top = '0';
+    img_large_div.style.left = '0';
+    img_large_div.style.zIndex = '100';
+    img_large_div.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    img_large_div.style.cursor = 'pointer';
+    img_large_div.onclick = function () {
+        img_large_div.remove();
+    }
+    document.body.appendChild(img_large_div);
+}
