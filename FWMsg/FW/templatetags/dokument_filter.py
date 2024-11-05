8ed0c15b91c04b93319f5144eada0037afe0b10c
Pirmin <pirmin@absinth.net>
2024-11-05 18:41:18 +0100

    ends with filter added

--- FWMsg/FW/templatetags/dokument_filter.py
+++ FWMsg/FW/templatetags/dokument_filter.py
@@ -6,3 +6,7 @@ register = template.Library()
 def starts_with(value, arg):
     print('value', value)
     return value.startswith(arg)
+
+@register.filter
+def ends_with(value, arg):
+    return value.endswith(arg)
