d4e3f5e7c14793fdcc6113263d253cc128b99a4f
Pirmin <pirmin@absinth.net>
2024-12-07 16:43:50 +0100

    added all org urls

--- FWMsg/ORG/urls.py
+++ FWMsg/ORG/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='org_home'),
+
+    path('add/<str:model_name>/', views.add_object, name='add_object'),
+    path('edit/<str:model_name>/<int:id>', views.edit_object, name='edit_object'),
+    path('list/<str:model_name>/', views.list_object, name='list_object'),
+    path('update/<str:model_name>/', views.update_object, name='update_object'),
+    path('delete/<str:model_name>/<int:id>', views.delete_object, name='delete_object'),
 ]
