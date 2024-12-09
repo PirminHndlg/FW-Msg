fbc0488d2213f3f3d6bd1d5e9eb111a6079174fb
Pirmin <pirmin@absinth.net>
2024-12-09 21:11:03 +0100

    list ampel and list aufgaben url added

--- FWMsg/ORG/urls.py
+++ FWMsg/ORG/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('list/<str:model_name>/', views.list_object, name='list_object'),
     path('update/<str:model_name>/', views.update_object, name='update_object'),
     path('delete/<str:model_name>/<int:id>', views.delete_object, name='delete_object'),
+    path('list-ampel/', views.list_ampel, name='list_ampel'),
+    path('list-aufgaben/', views.list_aufgaben, name='list_aufgaben'),
 ]
