3d08c196f89dac76ac6651d2891a7e4781f6462f
Pirmin <pirmin@absinth.net>
2024-12-28 23:53:43 +0100

    feat: Add custom template filters for organization-based styling
    - Implement hex to RGB color conversion filter
    - Add filters to fetch organization colors (primary and secondary)
    - Create filter for text color handling
    - Add helper filter to get user's organization
    - Include date formatting filter
    - Set default colors for non-authenticated users

--- FWMsg/FW/templatetags/base_filter.py
+++ FWMsg/FW/templatetags/base_filter.py
@@ -5,22 +5,24 @@ from ..models import CustomUser
 register = template.Library()
 
 @register.filter
+def hex_to_rgb(hex_code):
+    hex_code = hex_code.lstrip('#')
+    r, g, b = tuple(int(hex_code[i:i+2], 16) for i in (0, 2, 4))
+    return f"{r}, {g}, {b}"
+
+@register.filter
 def get_color(user):
     org = get_org(user)
     if org:
-        color = org.farbe
-    else:
-        color = 'green'
-    return color
+        return org.farbe
+    return '#007bff'
 
 @register.filter
 def get_secondary_color(user):
     org = get_org(user)
     if org:
-        color = org.farbe
-    else:
-        color = 'green'
-    return color
+        return org.farbe
+    return '#10700f'
 
 @register.filter
 def get_text_color(user):
