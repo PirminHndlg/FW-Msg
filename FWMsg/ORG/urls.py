39baa234569390eb581f2fdc42641a46548a10d7
Pirmin <pirmin@absinth.net>
2025-01-11 14:54:44 +0100

    feat: Update organization admin URL patterns

--- FWMsg/ORG/urls.py
+++ FWMsg/ORG/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('update/<str:model_name>/', views.update_object, name='update_object'),
     path('delete/<str:model_name>/<int:id>', views.delete_object, name='delete_object'),
     path('list-ampel/', views.list_ampel, name='list_ampel'),
+    path('list-ampel/<int:fid>', views.list_ampel_history, name='list_ampel_history'),
     path('list-aufgaben/', views.list_aufgaben, name='list_aufgaben'),
+    path('aufgaben-assign/', views.aufgaben_assign, name='aufgaben_assign'),
+    path('aufgaben-assign/<str:jahrgang>', views.aufgaben_assign, name='aufgaben_assign'),
 ]
