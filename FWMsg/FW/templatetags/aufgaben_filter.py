4c27a8f0a5322a969ad3ae7e0968244fa541e87d
Pirmin <pirmin@absinth.net>
2025-01-11 14:54:44 +0100

    feat: Add custom filters for task display

--- FWMsg/FW/templatetags/aufgaben_filter.py
+++ FWMsg/FW/templatetags/aufgaben_filter.py
@@ -9,21 +9,28 @@ register = template.Library()
 @register.filter
 def generate_html_for_aufgaben(aufgaben):
     html_content = ""
+    html_content += '<div class="row row-cols-1 row-cols-md-3 g-4">'
     for aufgabe in aufgaben:
         detail_url = reverse("aufgaben_detail", args=[aufgabe.aufgabe.id])
-        html_content += '<div class="aufgabe_div">'
-        html_content += f'<h4>{aufgabe.aufgabe.name}</h4>'
-        html_content += f'<p>{aufgabe.aufgabe.beschreibung}</p>'
+        html_content += '<div class="col">'
+        html_content += '<div class="card h-100">'
+        html_content += '<div class="card-body">'
+        html_content += f'<h4 class="card-title">{aufgabe.aufgabe.name}</h4>'
+        html_content += f'<p class="card-text">{aufgabe.aufgabe.beschreibung}</p>'
         if aufgabe.faellig:
-            html_content += f'<p>Fällig: {aufgabe.faellig.strftime("%d.%m.%Y")}</p>'
+            html_content += f'<p class="card-text"><small class="text-muted">Fällig: {aufgabe.faellig.strftime("%d.%m.%Y")}</small></p>'
+
+        if aufgabe.faellig and datetime.combine(aufgabe.faellig, datetime.min.time(), timezone.utc) < datetime.now(timezone.utc) and not aufgabe.erledigt and not aufgabe.pending:
+            html_content += '<p class="card-text"><span class="badge bg-danger">Überfällig</span></p>'
 
         if aufgabe.erledigt:
-            html_content += '<p>Erledigt</p>'
+            html_content += '<p class="card-text"><span class="badge bg-success">Erledigt</span></p>'
         elif aufgabe.pending:
-            html_content += '<p>Wird bearbeitet</p>'
-            html_content += f'<a href="{detail_url}">Bearbeiten</a>'
+            html_content += '<p class="card-text"><span class="badge bg-warning text-dark">Wird bearbeitet</span></p>'
+            html_content += f'<a href="{detail_url}" class="btn btn-primary text-white">Bearbeiten</a>'
         else:
-            html_content += f'<a href="{detail_url}">Jetzt erledigen</a>'
+            html_content += f'<a href="{detail_url}" class="btn btn-success text-white">Jetzt erledigen</a>'
 
-        html_content += '</div>'
+        html_content += '</div></div></div>'
+    html_content += '</div>'
     return mark_safe(html_content)
