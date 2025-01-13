6f94e69d94718efb1f7734b1b3aafc6498fda2ac
Pirmin <pirmin@absinth.net>
2025-01-13 11:49:04 +0100

    Add dynamic filtering system and form mappings

--- FWMsg/ORG/forms.py
+++ FWMsg/ORG/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db import models
 
 from FW import models as FWmodels
 from . import models as ORGmodels
@@ -30,6 +31,19 @@ class AddAufgabenprofilForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class AddFreiwilligerAufgabenForm(forms.ModelForm):
+    class Meta:
+        model = FWmodels.FreiwilligerAufgaben
+        fields = '__all__'
+        exclude = ['org']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'freiwilliger' in self.fields:
+            self.fields['freiwilliger'].disabled = True
+        if 'aufgabe' in self.fields:
+            self.fields['aufgabe'].disabled = True
 
 
 class AddKirchenzugehoerigkeitForm(forms.ModelForm):
@@ -90,6 +104,59 @@ class AddNotfallkontaktForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
 
+# Define which fields should be filterable for each model
+filterable_fields = {
+    FWmodels.Freiwilliger: ['jahrgang', 'einsatzland', 'entsendeform', 'kirchenzugehoerigkeit'],
+    FWmodels.Aufgabe: ['faellig_art', 'mitupload'],
+    FWmodels.Einsatzstelle: ['einsatzland'],
+    FWmodels.Notfallkontakt: ['freiwilliger'],
+    FWmodels.FreiwilligerAufgaben: ['freiwilliger', 'aufgabe', 'erledigt'],
+    # FWmodels.Aufgabenprofil: ['aufgaben__aufgabe'],
+}
+
+class FilterForm(forms.Form):    
+    def __init__(self, model_class, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Get the list of filterable fields for this model
+        allowed_fields = filterable_fields.get(model_class, [])
+        
+        # Add filter fields based on model fields, but only for allowed fields
+        for field in model_class._meta.fields:
+            if field.name not in allowed_fields:
+                continue
+                
+            if isinstance(field, (models.CharField, models.TextField)):
+                self.fields[f'filter_{field.name}'] = forms.CharField(
+                    required=False,
+                    label=field.verbose_name,
+                    widget=forms.TextInput(attrs={'class': 'form-control'})
+                )
+            elif isinstance(field, models.BooleanField):
+                self.fields[f'filter_{field.name}'] = forms.ChoiceField(
+                    required=False,
+                    label=field.verbose_name,
+                    choices=[('', '---'), ('true', 'Yes'), ('false', 'No')],
+                    widget=forms.Select(attrs={'class': 'form-control'})
+                )
+            elif isinstance(field, models.DateField):
+                self.fields[f'filter_{field.name}_from'] = forms.DateField(
+                    required=False,
+                    label=f'{field.verbose_name} from',
+                    widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+                )
+                self.fields[f'filter_{field.name}_to'] = forms.DateField(
+                    required=False,
+                    label=f'{field.verbose_name} to',
+                    widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+                )
+            elif isinstance(field, models.ForeignKey):
+                self.fields[f'filter_{field.name}'] = forms.ModelChoiceField(
+                    required=False,
+                    queryset=field.related_model.objects.all(),
+                    label=field.verbose_name,
+                    widget=forms.Select(attrs={'class': 'form-control'})
+                )
 
 model_to_form_mapping = {
     FWmodels.Einsatzland: AddEinsatzlandForm,
@@ -100,5 +167,6 @@ model_to_form_mapping = {
     FWmodels.Jahrgang: AddJahrgangForm,
     FWmodels.Kirchenzugehoerigkeit: AddKirchenzugehoerigkeitForm,
     FWmodels.Notfallkontakt: AddNotfallkontaktForm,
-    FWmodels.Entsendeform: AddEntsendeformForm
+    FWmodels.Entsendeform: AddEntsendeformForm,
+    FWmodels.FreiwilligerAufgaben: AddFreiwilligerAufgabenForm
 }
