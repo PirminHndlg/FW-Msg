d1367e91ddb3607c26c4815660a53a574c75d8b3
Pirmin <pirmin@absinth.net>
2025-01-13 11:51:48 +0100

    Enhance template filters for model field handling

--- FWMsg/ORG/templatetags/list_objects_filters.py
+++ FWMsg/ORG/templatetags/list_objects_filters.py
@@ -2,17 +2,41 @@ import datetime
 
 from django import template
 from datetime import date
+from django.db.models import ManyToManyField
 
 register = template.Library()
 
 
 @register.filter
-def get_attribute(obj, attr_name):
-    """Safely retrieve an attribute from an object."""
+def get_attribute(obj, attr):
+    """Get an attribute of an object by name."""
+    return getattr(obj, attr, '')
+
+@register.filter
+def getattribute(obj, attr):
+    """Get an attribute of an object by name."""
+    return getattr(obj, attr, '')
+
+@register.filter
+def get_fields(obj):
+    """Get the fields of an object."""
+    return obj.fields
+
+@register.filter
+def is_many_to_many(obj, field_name):
+    """Check if a field is a many-to-many relationship."""
     try:
-        data = getattr(obj, attr_name)
-        if type(data) == date:
-            return data.strftime('%d.%m.%Y')
-        return data
-    except AttributeError:
-        return None
+        field = obj._meta.get_field(field_name)
+        return isinstance(field, ManyToManyField)
+    except:
+        return False
+
+@register.filter
+def get_field_type(obj, field_name):
+    """Return the field type name for a given model field."""
+    try:
+        field = obj._meta.get_field(field_name)
+        return field.get_internal_type()
+    except:
+        return ''
+        
