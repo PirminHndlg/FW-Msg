41d13377ad096d36a1a6bbe4d012d20a7cd69682
Pirmin <pirmin@absinth.net>
2025-01-26 18:56:41 +0100

    added url for single login, rename index to index_home

--- FWMsg/Home/urls.py
+++ FWMsg/Home/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index_home'),
     path('index', views.index, name='index'),
+    path('first_login', views.first_login, name='first_login'),
 ]
