7bc3e2e9a3f578be4484027129aad099b2b5b170
Pirmin <pirmin@absinth.net>
2025-01-26 18:54:51 +0100

    added custom user to global models

--- FWMsg/FW/models.py
+++ FWMsg/FW/models.py
@@ -5,6 +5,7 @@ from PIL import Image  # Make sure this is from PIL, not Django models
 from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 from ORG.models import Organisation
+from Global.models import CustomUser
 from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from django.core.files.base import ContentFile
@@ -57,34 +58,6 @@ def calculate_small_image(image):
     img.save(img_io, format=format, quality=85)
     return ContentFile(img_io.getvalue(), name=image.name.split('/')[-1])
 
-
-class CustomUser(models.Model):
-    ROLE_CHOICES = [
-        ('A', 'Admin'),
-        ('O', 'Organisation'),
-        ('T', 'Team'),
-        ('F', 'Freiwillige:r'),
-    ]
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    org = models.ForeignKey(Organisation, on_delete=models.CASCADE, verbose_name='Organisation')
-    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default='F', verbose_name='Rolle')
-    profil_picture = models.ImageField(upload_to='profil_picture/', blank=True, null=True, verbose_name='Profilbild')
-
-    def __str__(self):
-        return self.user.username
-    
-@receiver(post_save, sender=CustomUser)
-def post_save_handler(sender, instance, created, **kwargs):
-    if instance.profil_picture and not hasattr(instance, '_processing_profil_picture'):
-        instance._processing_profil_picture = True
-        instance.profil_picture = calculate_small_image(instance.profil_picture)
-        instance.save()
-        delattr(instance, '_processing_profil_picture')
-
-# Add property to User model to access org
-User.add_to_class('org', property(lambda self: self.customuser.org if hasattr(self, 'customuser') else None))
-User.add_to_class('role', property(lambda self: self.customuser.role if hasattr(self, 'customuser') else None))
 
 class Entsendeform(models.Model):
     org = models.ForeignKey(Organisation, on_delete=models.CASCADE, verbose_name='Organisation')
