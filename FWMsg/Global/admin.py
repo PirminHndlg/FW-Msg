dfeddf7c572d45281e87a1de8483bf3ccc282aa6
Pirmin <pirmin@absinth.net>
2025-02-12 23:41:08 +0100

    added a button to admin to manually send mails

--- FWMsg/Global/admin.py
+++ FWMsg/Global/admin.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import path
+from django.contrib import messages
+from django.utils.html import format_html
 from .models import CustomUser, Feedback, KalenderEvent
+from FWMsg.celery import send_email_aufgaben_daily
 
 # Register your models here.
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    search_fields = ['user']
+    search_fields = ['user__username', 'user__email']
     actions = ['send_registration_email']
     list_filter = [('einmalpasswort', admin.EmptyFieldListFilter)]
+    list_display = ('user', 'org', 'role')
+    list_filter = ('org', 'role')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('send_daily_emails/', 
+                 self.admin_site.admin_view(self.send_daily_emails), 
+                 name='send_daily_emails'),
+        ]
+        return custom_urls + urls
 
     def send_registration_email(self, request, queryset):
         for customuser in queryset:
             customuser.send_registration_email()
+
+    def send_daily_emails(self, request):
+        try:
+            response = send_email_aufgaben_daily()
+            self.message_user(request, f'Daily task emails have been sent successfully. {response}', messages.SUCCESS)
+        except Exception as e:
+            self.message_user(request, f'Error sending emails: {str(e)}', messages.ERROR)
+        return HttpResponseRedirect("../")
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_send_emails_button'] = True
+        return super().changelist_view(request, extra_context=extra_context)
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
