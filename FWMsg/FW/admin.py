ffb820789d4bdf30144fd093f923f5d80f2c0a68
Pirmin <pirmin@absinth.net>
2025-02-15 10:13:17 +0100

    add make anonym

--- FWMsg/FW/admin.py
+++ FWMsg/FW/admin.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+import random
 from django.contrib import admin
 from .models import Freiwilliger, Entsendeform, Einsatzland, Einsatzstelle, Notfallkontakt, Post, Aufgabe, \
     Aufgabenprofil, FreiwilligerAufgabenprofil, Ampel, FreiwilligerAufgaben, Jahrgang, \
@@ -8,7 +10,7 @@ from .models import Freiwilliger, Entsendeform, Einsatzland, Einsatzstelle, Notf
 @admin.register(Freiwilliger)
 class FreiwilligerAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
-    actions = ['send_register_email', 'give_user_name']
+    actions = ['send_register_email', 'give_user_name', 'anonymize_user']
 
     def send_register_email(self, request, queryset):
         for freiwilliger in queryset:
@@ -21,6 +23,23 @@ class FreiwilligerAdmin(admin.ModelAdmin):
             freiwilliger.user.last_name = freiwilliger.last_name
             freiwilliger.user.save()
 
+    def anonymize_user(self, request, queryset):
+        for freiwilliger in queryset:
+            random_date_in_2007 = datetime(2007, 1, 1).date() + timedelta(days=random.randint(0, 365))
+            # Replace umlauts in name parts
+            first = freiwilliger.first_name.replace('ä','ae').replace('ö','oe').replace('ü','ue').replace('ß','ss')
+            last = freiwilliger.last_name.replace('ä','ae').replace('ö','oe').replace('ü','ue').replace('ß','ss')
+            freiwilliger.user.email = f'{first}.{last}@p0k.de'
+            freiwilliger.email = f'{first}.{last}@p0k.de'
+            freiwilliger.geburtsdatum = random_date_in_2007
+            freiwilliger.phone = None
+            freiwilliger.phone_einsatzland = None
+            freiwilliger.strasse = None
+            freiwilliger.plz = None
+            freiwilliger.ort = None
+            freiwilliger.country = None
+            freiwilliger.save()
+            freiwilliger.user.save()
 
 @admin.register(Entsendeform)
 class EntsendeformAdmin(admin.ModelAdmin):
@@ -40,6 +59,16 @@ class EinsatzstelleAdmin(admin.ModelAdmin):
 @admin.register(Notfallkontakt)
 class NotfallkontaktAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
+    actions = ['anonymize_user']
+
+    def anonymize_user(self, request, queryset):
+        for notfallkontakt in queryset:
+            notfallkontakt.first_name = f'{notfallkontakt.first_name[0]} anonymisiert'
+            notfallkontakt.last_name = f'{notfallkontakt.last_name[0]} anonymisiert'
+            notfallkontakt.phone = None
+            notfallkontakt.phone_work = None
+            notfallkontakt.email = f'{notfallkontakt.first_name}.{notfallkontakt.last_name}@p0k.de'
+            notfallkontakt.save()
 
 
 @admin.register(Post)
--- FWMsg/ORG/admin.py
+++ FWMsg/ORG/admin.py
@@ -21,6 +21,16 @@ class DokumentColorAdmin(admin.ModelAdmin):
 @admin.register(Referenten)
 class ReferentenAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'email', 'phone']
+    actions = ['anonymize_user']
+
+    def anonymize_user(self, request, queryset):
+        for referent in queryset:
+            referent.email = f'{referent.first_name[0]}.{referent.last_name[0]}@p0k.de'
+            referent.user.email = f'{referent.first_name[0]}.{referent.last_name[0]}@p0k.de'
+            referent.phone_work = None
+            referent.phone_mobil = None
+            referent.save()
+            referent.user.save()
 
 @admin.register(JahrgangTyp)
 class JahrgangTypAdmin(admin.ModelAdmin):
