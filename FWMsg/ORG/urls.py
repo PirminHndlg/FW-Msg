f0bedb48becbb42d871c1d23b1da233cd3bf97da
Pirmin <pirmin@absinth.net>
2025-05-12 15:50:33 +0200

    removed no longer needed templates, views and urls

--- FWMsg/ORG/urls.py
+++ FWMsg/ORG/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     
     path('download-aufgabe/<int:id>', views.download_aufgabe, name='download_aufgabe'),
     path('download-bild/<int:id>', views.download_bild_as_zip, name='download_bild_as_zip'),
-    path('list-bilder/', views.list_bilder, name='list_bilder'),
     path('statistik/', views.statistik, name='statistik'),
 
     path('nginx-statistic/', views.nginx_statistic, name='nginx_statistic'),
