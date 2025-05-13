beb865182cafd98c469624b2b63ff4a749a04247
Pirmin <pirmin@absinth.net>
2025-05-13 14:11:31 +0200

    Renamed names of urls with team as prefix

--- FWMsg/TEAM/urls.py
+++ FWMsg/TEAM/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.home, name='team_home'),
     path('contacts/', views.contacts, name='team_contacts'),
     path('ampelmeldung/', views.ampelmeldung, name='team_ampelmeldung'),
-    path('einsatzstellen/', views.einsatzstellen, name='einsatzstellen'),
-    path('einsatzstellen/save/<int:stelle_id>/', views.save_einsatzstelle_info, name='save_einsatzstelle_info'),
-    path('laender/', views.laender, name='laender'),
-    path('laender/save/<int:land_id>/', views.save_land_info, name='save_land_info'),
+    path('einsatzstellen/', views.einsatzstellen, name='team_einsatzstellen'),
+    path('einsatzstellen/save/<int:stelle_id>/', views.save_einsatzstelle_info, name='team_save_einsatzstelle_info'),
+    path('laender/', views.laender, name='team_laender'),
+    path('laender/save/<int:land_id>/', views.save_land_info, name='team_save_land_info'),
 ]
