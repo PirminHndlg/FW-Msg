dcca48c1fb4c2fb1675d681672aa47073b6c7ada
Pirmin <pirmin@absinth.net>
2025-05-16 10:39:30 +0200

    feat(BW): Add file upload functionality for application answers
    - Introduced new models for ApplicationFileQuestion and ApplicationAnswerFile to handle file uploads.
    - Created forms for uploading files and validating file types and sizes.
    - Updated views to manage file uploads, downloads, and deletions.
    - Enhanced templates to support file upload interfaces and display uploaded files.
    - Registered new models in the Django admin for management.
    - Updated URL patterns to include routes for file handling.
    - Improved home dashboard to reflect file upload progress alongside application questions.

--- FWMsg/BW/views.py
+++ FWMsg/BW/views.py
@@ -1,13 +1,15 @@
 from datetime import datetime
+import os
 from django.db import IntegrityError
+from django.http import FileResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from ORG.models import Organisation
-from .forms import CreateAccountForm, ApplicationAnswerForm
+from .forms import CreateAccountForm, ApplicationAnswerForm, ApplicationFileAnswerForm
 from FWMsg.decorators import required_role
-from .models import ApplicationQuestion, ApplicationAnswer, ApplicationText, Bewerber
-
+from .models import ApplicationQuestion, ApplicationAnswer, ApplicationText, Bewerber, ApplicationAnswerFile, ApplicationFileQuestion
+from django.contrib import messages
 # Create your views here.
 @login_required
 @required_role('B')
@@ -17,12 +19,20 @@ def home(request):
     total_questions = ApplicationQuestion.objects.filter(org=request.user.org).count()
     answered_questions = ApplicationAnswer.objects.filter(user=request.user).exclude(answer='').count()
     
+    total_file_questions = ApplicationFileQuestion.objects.filter(org=request.user.org).count()
+    answered_file_questions = ApplicationAnswerFile.objects.filter(user=request.user).exclude(file='').count()
+    
     context = {
         'application_text': application_text,
         'total_questions': total_questions,
         'answered_questions': answered_questions,
         'answered_questions_percentage': int(answered_questions / total_questions * 100) if total_questions > 0 else 0,
-        'open_questions': total_questions - answered_questions
+        'open_questions': total_questions - answered_questions,
+        
+        'total_file_questions': total_file_questions,
+        'answered_file_questions': answered_file_questions,
+        'answered_file_questions_percentage': int(answered_file_questions / total_file_questions * 100) if total_file_questions > 0 else 0,
+        'open_file_questions': total_file_questions - answered_file_questions
     }
     
     return render(request, 'homeBw.html', context)
@@ -57,8 +67,20 @@ def create_account(request, org_id):
 def bw_application_questions_list(request):
     questions = ApplicationQuestion.objects.filter(org=request.user.org).order_by('order')
     answers = ApplicationAnswer.objects.filter(user=request.user, question__in=questions).exclude(answer='')
+    
+    file_questions = ApplicationFileQuestion.objects.filter(org=request.user.org).order_by('order')
+    file_answers = ApplicationAnswerFile.objects.filter(user=request.user, file_question__in=file_questions)
+    
     answers_dict = {answer.question.id: answer for answer in answers}
-    return render(request, 'bw_application_questions_list.html', {'questions': questions, 'answers': answers_dict})
+    file_answers_dict = {answer.file_question.id: answer for answer in file_answers}
+    
+    context = {
+        'questions': questions,
+        'answers': answers_dict,
+        'file_answers': file_answers_dict
+    }
+    
+    return render(request, 'bw_application_questions_list.html', context)
 
 @login_required
 # @required_role('B')
@@ -97,3 +119,62 @@ def bw_application_complete(request):
     bewerber.abgeschlossen_am = datetime.now()
     bewerber.save()
     return redirect('bw_home')
+
+@login_required
+@required_role('B')
+def bw_application_files_list(request):
+    file_questions = ApplicationFileQuestion.objects.filter(org=request.user.org).order_by('order')
+    file_answers = ApplicationAnswerFile.objects.filter(user=request.user, file_question__in=file_questions)
+    
+    # Create a dictionary with question IDs as keys and answers as values
+    answers_dict = {answer.file_question.id: answer for answer in file_answers}
+    
+    context = {
+        'file_questions': file_questions,
+        'file_answers': answers_dict
+    }
+    
+    return render(request, 'bw_application_files_list.html', context)
+
+@login_required
+# @required_role('B')
+def bw_application_file_answer(request, file_question_id):
+    file_question = ApplicationFileQuestion.objects.get(org=request.user.org, id=file_question_id)
+    answer = ApplicationAnswerFile.objects.filter(user=request.user, file_question=file_question).first()
+    if len(request.FILES) == 1:
+        form = ApplicationFileAnswerForm(request.POST or None, request.FILES or None, user=request.user, file_question=file_question, instance=answer)
+    else:
+        form = ApplicationFileAnswerForm(request.POST or None, user=request.user, file_question=file_question, instance=answer)
+
+    if request.method == 'POST' and form.is_valid() and len(request.FILES) == 1:
+        form.save()
+        return redirect('bw_application_files_list')
+    elif request.method == 'POST' and form.is_valid() and len(request.FILES) == 0:
+        return redirect('bw_application_files_list')
+    
+    return render(request, 'bw_application_file_answer.html', {'form': form, 'file_question': file_question, 'answer': answer})
+
+@login_required
+# @required_role('B')
+def bw_application_file_answer_download(request, file_answer_id):
+    try:
+        file_answer = ApplicationAnswerFile.objects.get(id=file_answer_id, user=request.user)
+    except ApplicationAnswerFile.DoesNotExist:
+        messages.error(request, 'Datei nicht gefunden')
+        return redirect('bw_application_files_list')
+    
+    if file_answer.file and os.path.exists(file_answer.file.path):
+        response = FileResponse(file_answer.file)
+        response['Content-Disposition'] = f'attachment; filename="{file_answer.file.name}"'
+        return response
+    else:
+        messages.error(request, 'Datei nicht gefunden')
+        return redirect('bw_application_files_list')
+
+@login_required
+# @required_role('B')
+def bw_application_file_answer_delete(request, file_answer_id):
+    file_answer = ApplicationAnswerFile.objects.get(id=file_answer_id, user=request.user)
+    file_answer.delete()
+    messages.success(request, 'Datei wurde erfolgreich gelöscht')
+    return redirect('bw_application_files_list')
