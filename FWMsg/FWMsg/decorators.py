d17e3cfe3512b5d571ef1b90e49ff8c92b80cec7
Pirmin <pirmin@absinth.net>
2025-06-03 11:34:43 +0200

    fix(decorators): Allow superusers to bypass role checks
    - Updated the role validation in the required_role decorator to permit superusers access, enhancing permission handling for privileged users.

--- FWMsg/FWMsg/decorators.py
+++ FWMsg/FWMsg/decorators.py
@@ -19,7 +19,7 @@ def required_role(roles):
         # @user_passes_test(lambda u: u.is_authenticated and hasattr(u, 'customuser'))
         def _wrapped_view(request, *args, **kwargs):
             try:
-                if not request.user.customuser.person_cluster.view in roles and roles != '':
+                if not request.user.customuser.person_cluster.view in roles and roles != '' and not request.user.is_superuser:
                     raise PermissionDenied
             except:
                 raise PermissionDenied
