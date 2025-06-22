8baf43ccfeb29daf6b202c2765878cb9940ecafa
Pirmin <pirmin@absinth.net>
2025-06-22 12:14:56 +0200

    feat(password_change): Implement password change functionality and associated template
    - Added a new view for password change, utilizing Django's SetPasswordForm for secure password updates.
    - Introduced a new template for the password change page, enhancing user experience with a responsive design and clear instructions.
    - Integrated password validation to ensure compliance with security requirements before allowing changes.

--- FWMsg/Home/forms.py
+++ FWMsg/Home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
--- FWMsg/Home/urls.py
+++ FWMsg/Home/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('first_login', views.first_login, name='first_login'),
     path('first_login/<str:username>/<str:einmalpasswort>', views.first_login, name='first_login_with_params'),
     path('first_login/<str:username>', views.first_login, name='first_login_with_username'),
+    path('password_change', views.password_change, name='password_change'),
     path('password_reset', views.password_reset, name='password_reset'),
     path('maintenance', views.maintenance, name='maintenance'),
 ]
