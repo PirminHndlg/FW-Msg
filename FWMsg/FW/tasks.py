840fb2d5c3f6c10abb448ad2f7cbc8fbe50f9c07
Pirmin <pirmin@absinth.net>
2025-07-03 17:24:30 +0200

    fix(tasks): Update email registration task to use Django URL reversing
    - Refactored the `send_register_email_task` to utilize Django's `reverse` function for generating the action URL, improving maintainability and readability.
    - Adjusted the formatting of the base64 image encoding for consistency.
    - Enhanced the email content generation for better clarity and structure.

--- FWMsg/FW/tasks.py
+++ FWMsg/FW/tasks.py
@@ -2,10 +2,9 @@ import base64
 from datetime import datetime, timedelta
 from django.db.models import Q
 from celery import shared_task
-
+from django.urls import reverse
 from django.contrib.auth.models import User
 from Global.send_email import send_mail_smtp, format_register_email_fw
-
 
 
 @shared_task
@@ -14,15 +13,20 @@ def send_register_email_task(user_id):
     org = user.customuser.org
 
     with open(org.logo.path, "rb") as org_logo:
-        base64_image = base64.b64encode(org_logo.read()).decode('utf-8')
+        base64_image = base64.b64encode(org_logo.read()).decode("utf-8")
     org_name = org.name
     einmalpasswort = user.customuser.einmalpasswort
     user_name = f"{user.first_name} {user.last_name}"
     username = user.username
-    action_url = f'https://volunteer.solutions/first_login?username={username}&einmalpasswort={einmalpasswort}'
-    
-    email_content = format_register_email_fw(einmalpasswort, action_url, base64_image, org_name, user_name, username)
-    subject = f'Account erstellt: {user_name}'
+    action_url = f'https://volunteer.solutions{reverse(
+        "first_login_with_params",
+        kwargs={"username": username, "einmalpasswort": einmalpasswort},
+    )}'
+
+    email_content = format_register_email_fw(
+        einmalpasswort, action_url, base64_image, org_name, user_name, username
+    )
+    subject = f"Account erstellt: {user_name}"
     if send_mail_smtp(user.email, subject, email_content, reply_to=org.email):
         return True
     return False
