7e254d61e5e8618d2befc829ba5ba805a4c85c02
Pirmin <pirmin@absinth.net>
2025-09-04 15:14:55 +0200

    fix(migrations): Remove obsolete dependency in initial migration for BW app
    - Eliminated the outdated dependency on a previous migration in the ORG app, ensuring the initial migration for the BW app is streamlined and maintains integrity.

--- FWMsg/BW/migrations/0001_initial.py
+++ FWMsg/BW/migrations/0001_initial.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('ORG', '0034_dokumentcolor_historicaldokumentcolor_and_more'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
