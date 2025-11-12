b68bc2f7118d3c724061cffa8afcdf709a6acf81
Pirmin <pirmin@absinth.net>
2025-11-12 09:43:41 +0100

    feat(bw_filters, templates): Add Bewerber filtering functionality and update template structure
    - Introduced a new filter in bw_filters.py to retrieve Bewerber associated with interview persons.
    - Updated seminar_display_info.html to display Bewerber information conditionally based on user role, enhancing the user interface for viewing applications.
    - Commented out welcome sections in ehemalsHome.html and teamHome.html for future reference, improving template organization.

--- FWMsg/BW/templatetags/bw_filters.py
+++ FWMsg/BW/templatetags/bw_filters.py
@@ -1,7 +1,14 @@
 from django import template
+from BW.models import Bewerber
 
 register = template.Library()
 
 @register.filter
 def max_order(questions):
     return questions.order_by('order').last().order
+
+
+@register.filter
+def get_bewerbers_of_interviewperson(interview_persons):
+    bewerbers = Bewerber.objects.filter(interview_persons=interview_persons)
+    return bewerbers
