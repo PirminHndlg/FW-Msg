5bc19ed018ac74ce99d1b97d60669a8ca0463280
Pirmin <pirmin@absinth.net>
2025-11-27 15:55:46 +0100

    refactor(form_submission): Improve form submission handling by disabling submit button and removing console logs
    - Updated the form submission process to disable the submit button and display a spinner during submission for better user feedback.
    - Removed unnecessary console log statements to clean up the code and enhance performance.

--- FWMsg/Global/global-static/js/main.js
+++ FWMsg/Global/global-static/js/main.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const all_forms = document.querySelectorAll('form');
-    console.log(all_forms);
     for (const form of all_forms) {
-        console.log(form);
         form.addEventListener('submit', function () {
-            console.log('form submitted');
             const submitBtn = form.querySelector('button[type="submit"]');
             const spinner = document.createElement('span');
             spinner.className = 'spinner-border spinner-border-sm text-white';
             spinner.role = 'status';
             spinner.ariaHidden = 'true';
             submitBtn.innerHTML = spinner.outerHTML + submitBtn.innerHTML;
-            submitBtn.setAttribute('disabled', 'disabled');
+            submitBtn.classList.add('disabled');
         });
     }
 });
